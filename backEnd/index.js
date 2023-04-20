edfa569fda56fbdc5ea47f0032f0eda881da16a5
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-20 14:39:36 +0200

    env + config + db

---
+++ backEnd/index.js
@@ -0,0 +1,7 @@
+import './util/config.js'
+import express from 'express'     // zum server erstellen
+import cors from 'cors'          // für Sicherheit URL beschränken    // ! Secure HTTPS Cookies  credentails: true
+import morgan from 'morgan'     // für Logging
+import cookieParser from 'cookie-parser'     // auslesen von Cookies
+
+
