1856cad7c5623e29067a0b1c525a735f6dbb4f8c
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-22 12:50:55 +0200

    fE register

--- frontEnd/vite-project/src/App.jsx
+++ frontEnd/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import './App.css'
 
@@ -16,16 +16,49 @@ function App() {
       headers: {
         'content-type': 'application/json'          //  damit das Prasen mit express.json() funktioniert
       },
-      body:JSON.stringify({ user: 'testName', password: 'testPassword'})
+      body: JSON.stringify({ user: 'testName', password: 'testPassword' })
     })
     const data = await result.json()
     console.log(data)
   }
 
   const check = async () => {
-    const result = await fetch('http://localhost:9999/user',{
+    const result = await fetch('http://localhost:9999/user', {
       credentials: 'include'
     })
+    const data = await result.json()
+    console.log(data)
+  }
+
+  const userRef = useRef()   // useRef anstatt useState, 
+  // damit nicht bei jedem tippen ein neu rendern stattfindet
+  const passwordRef = useRef()
+  const envURL = 'http://localhost:9999'
+  const envPath = '/register'
+  const url = envURL + envPath
+  const register = async () => {
+
+ 
+
+    // holt sich den Wert vom inputFeld user und password
+    const user = userRef.current.value      // holen des inputs 
+    const password = passwordRef.current.value
+    try {
+      const result = await fetch(url, {
+        method: 'POST',
+        credentials: 'include',        // wichtig damit Secure Cookies abgefragt und mitgeschickt werden
+        headers: {
+          'content-type': 'application/json'    // damit express.json() Parsen kann
+        },
+        body: JSON.stringify({ user, password })  //?   // input Felder in ein Objekt und mitschicken
+      })
+      const data = await result.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+
+
   }
 
   return (
@@ -33,6 +66,13 @@ function App() {
 
       <button onClick={login} >login</button>
       <button onClick={check}>check</button>
+
+      <button onClick={register}>register</button>
+      <label htmlFor='user'>Register</label>
+      <input ref={userRef} type="text" placeholder='user' name='user' required></input>
+      <label htmlFor='password'>Password</label>
+      <input ref={passwordRef} type="text" placeholder='password' name='password' required></input>
+
 
     </>
   )
