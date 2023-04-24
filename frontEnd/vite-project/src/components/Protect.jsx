5b1c8604de601cf898f90e0761554da391c25002
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-24 14:50:31 +0200

    fE  Protect bauen

--- frontEnd/vite-project/src/components/Protect.jsx
+++ frontEnd/vite-project/src/components/Protect.jsx
@@ -1,29 +1,39 @@
 import { Navigate, Outlet } from "react-router-dom"
 
 // rfac
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const Protect = () => {
 
-    const verifyToken = async () => {
-        try {
-            const result = await fetch('http://localhost:9999/userValidate', {
-                credentials: 'include'
-            })
-            if (result.ok) {
-                return <Outlet></Outlet> // damit wird in die Erlaubten Routen die Protected sind weitergeleitet
 
+    // useEffect um verifyToken herum bauen
+    // und noch einen State ab schon ein Ergebniss vom fetch da ist
 
-            } else {
-                return false
-                // Seite wo soll er noch so hin wenn nix ist
+    useEffect(() => {
+
+        const verifyToken = async () => {
+            try {
+                const result = await fetch('http://localhost:9999/userValidate', {
+                    credentials: 'include'
+                })
+                if (result.ok) {
+                    console.log(result)
+                    return <Outlet></Outlet> // damit wird in die Erlaubten Routen die Protected sind weitergeleitet
+
+                } else {
+                    return false
+                    // Seite wo soll er noch so hin wenn nix ist
+                }
+            } catch (err) {
+                console.log(err)
             }
-        } catch (err) {
-            console.log(err)
+
         }
+        verifyToken()
 
-    }
+    }, [])
 
 
-   
+
+
 }
