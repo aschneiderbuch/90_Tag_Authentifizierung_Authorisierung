6485917e10812188c321e9e3f9ca8c32277e8f63
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-24 13:23:30 +0200

    fE login Input

--- frontEnd/vite-project/src/App.jsx
+++ frontEnd/vite-project/src/App.jsx
@@ -9,14 +9,21 @@ function App() {
     console.log(document.cookie)
   }, [])
 
+  const userLoginRef = useRef()
+  const passwordLoginRef = useRef()
+
   const login = async () => {
+
+    const userLogin = userLoginRef.current.value
+    const passwordLogin = passwordLoginRef.current.value
+
     const result = await fetch('http://localhost:9999/login', {
       method: 'POST',
       credentials: 'include',         // ! wichtig, damit die Secure Cookies abgefragt und mitgeschickt werden
       headers: {
         'content-type': 'application/json'          //  damit das Prasen mit express.json() funktioniert
       },
-      body: JSON.stringify({ user: 'testName', password: 'testPassword' })
+      body: JSON.stringify({ user: userLogin, password: passwordLogin })  //?   // input Felder in ein Objekt und mitschicken
     })
     const data = await result.json()
     console.log(data)
@@ -78,6 +85,10 @@ function App() {
     <>
 
       <button onClick={login} >login</button>
+      <label htmlFor="userLogin">userLogin</label>
+      <input ref={userLoginRef} type="text" name="userLogin" id="userLogin" placeholder='userLogin' required/>
+      <label htmlFor="passwordLogin">passwordLogin</label>
+      <input ref={passwordLoginRef} type="text" name='passwordLogin' id='passwordLogin' placeholder="passwordLogin" required />
       <button onClick={check}>check</button>
 
       <button onClick={register}>register</button>
